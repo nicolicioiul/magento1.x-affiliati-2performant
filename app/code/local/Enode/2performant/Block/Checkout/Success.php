abe442f10975a3fd78e51615c2b3eff466ce5465
Nicolicioiu Liviu <liviu.nicolicioiu@epoint.ro>
2018-03-05 19:05:56 +0200

    Update token and enable variable.

--- app/code/local/Enode/2performant/Block/Checkout/Success.php
+++ app/code/local/Enode/2performant/Block/Checkout/Success.php
@@ -33,7 +33,7 @@ class Enode_2performant_Block_Checkout_Success extends Mage_Core_Block_Template
               '<iframe height="1" width="1" scrolling="no" marginheight="0" ' .
               ' marginwidth="0" frameborder="0" ' .
               ' src="//event.2performant.com/events/salecheck?amount=__ADD_SALE_VALUE__&'
-              . 'campaign_unique=__campaign_unique__&confirm=__campaign_unique__&description=__ADD_DESCRIPTION__&'
+              . 'campaign_unique=__campaign_unique__&confirm=__confirm__&description=__ADD_DESCRIPTION__&'
               . 'transaction_id=__ADD_TRANSACTION_ID__"></iframe>');
         }
         return '';
@@ -44,12 +44,14 @@ class Enode_2performant_Block_Checkout_Success extends Mage_Core_Block_Template
      */
     protected function _beforeToHtml()
     {
-        $orderId = Mage::getSingleton('checkout/session')->getLastOrderId();
-        if ($orderId) {
-            /** @var Mage_Sales_Model_Order $order */
-            $order = Mage::getModel('sales/order')->load($orderId);
-            if ($order->getId()) {
-                $this->order = $order;
+        if (Mage::helper('e2performant')->isEnabled()) {
+            $orderId = Mage::getSingleton('checkout/session')->getLastOrderId();
+            if ($orderId) {
+                /** @var Mage_Sales_Model_Order $order */
+                $order = Mage::getModel('sales/order')->load($orderId);
+                if ($order->getId()) {
+                    $this->order = $order;
+                }
             }
         }
         return parent::_beforeToHtml();
